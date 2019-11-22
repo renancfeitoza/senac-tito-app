5896015a1dbb26bd0cf3979fbe5f9399cd4bab48
FELIPE ANDRE DA SILVA MELO <felipe.asmelo@senacsp.edu.br>
2019-11-22 19:45:04 -0300

    updated social network

--- front-end/src/pages/Login/components/form/index.js
+++ front-end/src/pages/Login/components/form/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import logo from '../../../../utils/IMG/Senac_logo.svg.png'
 import '../../../../utils/CSS/Login.css'
 import facebook from '../../../../utils/IMG/facebook-logo.svg'
 import instagram from '../../../../utils/IMG/instagram.svg'
 import linkedin from '../../../../utils/IMG/linkedin.svg'
 import twitter from '../../../../utils/IMG/twitter.svg'
-import youtube from '../../../../utils/IMG/youtube.svg'
 import { messageError } from '../../../../components/ValidationRules'
 export const Form = ({
     login,
@@ -21,21 +19,19 @@ export const Form = ({
                     <i className="material-icons">menu</i>
                 </div>
                 <div className="redes-sociais">
-                    <Link to="#">
+                    <a href="https://www.facebook.com/senaclapatito/">
                         <img src={facebook} alt="ícone da cor preta do facebook"/>
-                    </Link>
-                    <Link to="#">
+                    </a>
+                    <a href="https://www.instagram.com/senacsaopaulo/">
                         <img src={instagram} alt="ícone da cor preta do instagram"/>
-                    </Link>
-                    <Link to="#"> 
+                    </a>
+                    <a href="https://www.linkedin.com/company/senac"> 
                          <img src={linkedin} alt="ícone da cor preta do linkedin"/>
-                    </Link>
-                    <Link to="#">
+                    </a>
+                    <a href="https://twitter.com/senacsaopaulo">
                         <img src={twitter} alt="ícone da cor preta do twitter"/>
-                    </Link>
-                    <Link to="#">
-                        <img src={youtube} alt="ícone da cor preta do youtube"/>
-                    </Link>
+                    </a>
+
                 </div>
             </nav>
                 <div className="logo">
