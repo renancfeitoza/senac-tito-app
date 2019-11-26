f7b0f6e9322627e74f2e99463f70ee0805157468
felipe <profissionalf.andre@gmail.com>
2019-11-26 15:56:15 -0300

    updated login form screen

--- front-end/src/pages/Login/components/form/index.js
+++ front-end/src/pages/Login/components/form/index.js
@@ -1,12 +1,8 @@
-import React from 'react'
+    import React from 'react'
 import logo from '../../../../utils/IMG/Senac_logo.svg.png'
 import '../../../../utils/CSS/Login.css'
-import facebook from '../../../../utils/IMG/facebook-logo.svg'
-import instagram from '../../../../utils/IMG/instagram.svg'
-import linkedin from '../../../../utils/IMG/linkedin.svg'
-import twitter from '../../../../utils/IMG/twitter.svg'
 import { messageError } from '../../../../components/ValidationRules'
-import '../../../../utils/CSS/hamburgers.css'
+import { Link } from 'react-router-dom'
 export const Form = ({
     login,
     handleChangeLogin,
@@ -14,76 +10,54 @@ export const Form = ({
     errors,
 } = this.props) =>{
     return (
-        <React.Fragment>
-           <div className="navBar">
-                    <div className="redes-sociais">
-                        <a href="https://www.facebook.com/senaclapatito/">
-                            <img src={facebook} alt="ícone com cor de fundo preta do facebook"/>
-                        </a>
-                        <a href="https://www.instagram.com/senacsaopaulo/">
-                            <img src={instagram} alt="ícone com cor de fundo preta do instagram"/>
-                        </a>
-                        <a href="https://www.linkedin.com/company/senac"> 
-                            <img src={linkedin} alt="ícone com cor de fundo preta do linkedin"/>
-                        </a>
-                        <a href="https://twitter.com/senacsaopaulo">
-                            <img src={twitter} alt="ícone com cor de fundo preta do twitter"/>
-                        </a>
-                  </div>
-                </div>
-                <input type="checkbox" id="check" />
-                <label for="check"><i className="material-icons">menu</i></label>
-                <div className="bar">
-                <nav>
-                    <a href="#">
-                    <div className="link">Principal</div>
-                    </a>
-                    <a href="#">
-                    <div className="link">Cursos</div>
-                    </a>
-                    <a href="#">
-                    <div className="link">?</div>
-                    </a>
-                    <a href="#">
-                    <div className="link">Contato</div>
-                    </a>
-                </nav>
-                </div>
-                <div className="logo">
-                <img src={logo} alt="Logotipo oficial do Senac Brasil" className="logo" />
-                </div>
-                <div className="formLogin">
-                <form action="#">
-                    <label htmlFor="email">E-mail</label>
-
-                    <div className="login">
-                    <span>
-                        <i className="material-icons">email</i>
-                    </span>
-                    <input 
-                        type="text" 
-                        placeholder="Seu E-mail" 
-                        id="email"
-                        aria-required="true" 
-                        aria-label="Digite aqui o email neste campo"
-                        placeholder="Digite seu email para fazer login"
-                        onChange={handleChangeLogin}
-                    />
-                    </div>
-                    {
-                        messageError(
-                            errors,
-                            login.email,
-                            true,
-                            undefined,
-                            undefined
-                        )
-                    }
-
-                    <button className="btn-entrar" onClick={handleSubmit} id="submit">Entrar</button>
-                </form>
-                </div>
-                <footer></footer>
-        </React.Fragment>
+        <>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check"><i className="material-icons">menu</i></label>
+      <div className="menu">
+        <nav>
+          <Link to="#">
+            <div className="link">Principal</div>
+          </Link>
+          <Link to="#">
+            <div className="link">Cursos</div>
+          </Link>
+          <Link to="#">
+            <div className="link">?</div>
+          </Link>
+          <Link to="#">
+            <div className="link">Contato</div>
+          </Link>
+        </nav>
+      </div>
+      <div className="formLogin">
+        <div className="logo">
+          <img src={logo} alt="" className="logo" />
+        </div>
+        <form action="#">
+          <div className="login">
+            <span>
+              <i className="material-icons">email</i>
+            </span>
+            <input 
+                type="text" 
+                placeholder="Digite seu email para logar no sistema" 
+                id="email" 
+                value={login.email}
+                onChange={handleChangeLogin}
+            />
+          </div>
+            {
+                messageError(
+                    errors,
+                    login.email,
+                    true,
+                    10,
+                    150
+                )
+            }
+          <button className="btn-entrar" onClick={handleSubmit} id="submit">Entrar</button>
+        </form>
+      </div>
+      </>
     )
 }
