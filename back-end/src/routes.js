d7acddfdc848e47544e6410a847ffc3d8cc79ad7
Renan <renan.cfeitoza@gmail.com>
2019-11-25 21:38:16 -0300

    Criando primeira rota teste com sequelize

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,27 +1,8 @@
 const express = require("express");
+const UsuarioController = require('./controller/UsuarioController')
 
 const routes = express.Router();
 
-routes.get('/api', (req, res) => {
-    res.send(`
-
-    <!DOCTYPE html>
-    <html>
-        <title>New Project Senac</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <body>
-            <div class="text-center">
-                <h1 class="text-center mt-5 display-1 font-weight-bolder">New Project Senac</h1>
-            </div>
-        </body> 
-    </html>
-
-    `)
-});
-
-
-
+routes.get('/', UsuarioController.store);
 
 module.exports = routes;
